359ca80ff76d08e85a99384fd84c4477cdd43e04
Adrian Cristea <cristea.adrian@icloud.com>
2022-10-19 11:12:35 +0100

    renamed repo and project to scotia-property-services

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-gray-50">
+    <div class="mx-auto max-w-7xl divide-y divide-gray-200 py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900">Frequently asked questions</h2>
+      <div class="mt-8">
+        <dl class="divide-y divide-gray-200">
+          <div v-for="faq in faqs" :key="faq.id" class="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+            <dt class="text-base font-medium text-gray-900 md:col-span-5">{{ faq.question }}</dt>
+            <dd class="mt-2 md:col-span-7 md:mt-0">
+              <p class="text-base text-gray-500">{{ faq.answer }}</p>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+const faqs = [
+  {
+    id: 1,
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  // More questions...
+]
+</script>
