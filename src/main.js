39998bbde701454798c744ca907b1ecf83bf3c15
Adrian Cristea <cristea.adrian@icloud.com>
2022-10-19 23:50:13 +0100

    Revert "added inter font locally"
    This reverts commit 26b862f8f922a77c81a9c3ff8b02018471dee2f0.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
+import "./style.css";
 import App from "./App.vue";
-import "@fontsource/inter/variable.css";
-import "./tailwind.css";
 
 createApp(App).mount("#app");
